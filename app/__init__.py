4125c3846eb6321ae28d53310c1ab6dcfac86e1c
Ritik488 <ritikmiglani488@gmail.com>
2020-11-07 19:58:44 +0530

    morse integrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_socketio import SocketIO
 
+
 socketio = SocketIO()
 
 
--- chat.py
+++ chat.py
@@ -1,7 +1,54 @@
 #!/bin/env python
 from app import create_app, socketio
+from flask import Flask, render_template, Response
+from morse_camera import Camera
+from morse_eyes import Detectmorse
+import time
+from morse_log import log
 
 app = create_app(debug=True)
+
+morse = Detectmorse()
+
+@app.route('/homepage')
+def index():
+    """Video streaming home page."""
+    return render_template('homepage.html')
+
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        png, L = morse.calculate(frame)
+        streambyte()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + png.tobytes() + b'\r\n')
+
+
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+
+@app.route('/yield')
+def streambyte():
+    L = morse.L
+    final = morse.final
+    #print("raghav",L)
+
+    b = (''.join(L)).encode('utf-8')
+    c = (final).encode('utf-8')
+    log(b)
+    log(c)
+    def events():
+        yield "data: %s %s\n\n" % (b,c)
+        time.sleep(1)  # an artificial delay
+    return Response(events(), content_type='text/event-stream')
+
 
 if __name__ == '__main__':
     socketio.run(app)
